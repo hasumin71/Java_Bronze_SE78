59f40b02e82bc16d8d99a54efa9075c8dec1c295
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-13 12:51:21 +0900

    test_6_16

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-// // 問６−１８
+// 問６−１８
 // class Test {
 //   static String lang = "C";
 //   public String operation = "Unix";
@@ -15,13 +15,26 @@
 //   }
 // }
 
-// 問６−１7
-class Test {
-  static int num;
-   void methodA() {num++;} //このメソッドがstaticじゃないからコンパイルエラー
-  static void methodB() {num++;}
-  public static void main(String[] args){
-    methodA(); methodB();
-    System.out.println(num);
+// // 問６−１7
+// class Test {
+//   static int num;
+//    void methodA() {num++;} //このメソッドがstaticじゃないからコンパイルエラー
+//   static void methodB() {num++;}
+//   public static void main(String[] args){
+//     methodA(); methodB();
+//     System.out.println(num);
+//   }
+// }
+
+//問６−１6
+class Test{
+  public static void main(String[] args){ //stringのあとに[]をつけないとコンパイルエラー 
+    int num;                              //voidをつけているので戻り値を返さない
+    num = 10;
+    calc(num);
+    System.out.println("num = " + num);
+  }
+  static void calc(int num){
+    num += 100; //ここの値は変数に代入されない
   }
 }
