41ffc5b259b54353769131c6577b25439350bbd6
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-16 17:04:33 +0900

    test_17

--- Test.java
+++ Test.java
@@ -139,15 +139,24 @@
 // }
 
 //16
+// class Test {
+//   public static void main(String[] args){
+//     int[] array1 = {1,2,3};
+//     int[] array2 = {4,5};
+//     for (int x = 0; x < array1.length; x++){
+//       System.out.print(array1[x]+ " ");
+//       for(int y = 0; y < array2.length; y++){
+//         System.out.print(array2[y] + " ");
+//       }
+//     }
+//   }
+// }
+
+//17
 class Test {
-  public static void main(String[] args){
-    int[] array1 = {1,2,3};
-    int[] array2 = {4,5};
-    for (int x = 0; x < array1.length; x++){
-      System.out.print(array1[x]+ " ");
-      for(int y = 0; y < array2.length; y++){
-        System.out.print(array2[y] + " ");
-      }
+  public static void main(String[] args){ //初めから判定がfalseでも
+    for(int i = 5; i < i++; i++){
+      System.out.println("Hi");
     }
   }
 }
